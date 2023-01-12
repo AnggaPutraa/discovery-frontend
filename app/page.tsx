7511ee863dea3042bc465500f5cb878b5b13a1d5
AnggaPutraa <yananggaa@gmail.com>
2023-01-12 13:30:28 +0700

    fix: remove dummy data

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import NewsList from "../components/NewsList";
 import { categories } from "../constants";
 import fetchNews from "../lib/request";
-import response from '../response.json';
 
 const Homepage = async () => {
 
-    const newsResponse: NewsResponse = response || await fetchNews(categories.join(','));
+    const newsResponse: NewsResponse = await fetchNews(categories.join(','));
 
     return (
         <section className="max-w-7xl mx-auto px-8 mt-4">
